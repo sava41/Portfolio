ec1bb51de738938eba73eab49efdeb4467699142
sava <sava41@gmail.com>
2022-01-16 11:59:20 -0500

    fixed tile grid, improved layout

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,51 +1,63 @@
-$(document).ready(function () {
+// init Isotope
+var $grid = null;
+$(window).on('load resize', function () {
+    $grid = $('.portfolio-container').isotope({
+        itemSelector: '.mix',
+        transitionDuration: 0,
+        percentPosition: true,
+        masonry: {
+            columnWidth: '.mix'
+        }
+    })
+});
 
-    'use strict';
-
-    if ($('.portfolio-container').length) {
-        $(window).on('load', function () {
-
-            var isotop = $('.portfolio-container').isotope({
-                itemSelector: '.mix',
-                transitionDuration: 0
-            });
-
-            $('.portfolio-controls').on('click', 'button', function () {
-                var filterValue = $(this).attr('data-filter');
-                isotop.isotope({
-                    filter: filterValue
-                });
-            });
-
-            $('.portfolio-controls').each(function (i, buttonGroup) {
-                var $buttonGroup = $(buttonGroup);
-                $buttonGroup.on('click', 'button', function () {
-                    $buttonGroup.find('.active').removeClass('active');
-                    $(this).addClass('active');
-                });
-            });
-
-        });
+// filter functions
+var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function () {
+        var number = $(this).find('.number').text();
+        return parseInt(number, 10) > 50;
+    },
+    // show if name ends with -ium
+    ium: function () {
+        var name = $(this).find('.name').text();
+        return name.match(/ium$/);
     }
+};
 
+// bind filter button click
+$('.portfolio-controls').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[filterValue] || filterValue;
+    $grid.isotope({
+        filter: filterValue
+    });
+});
+
+// change is-checked class on buttons
+$('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function () {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+    });
 });
 
 // smooth scroll
-
-$('a.smooth ').on('click',function (e) {
+$('a.smooth ').on('click', function (e) {
     // e.preventDefault();
     var target = this.hash,
-    $target = $(target);
+        $target = $(target);
 
-   $('html, body').stop().animate({
-     'scrollTop': $target.offset().top-1
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 1
     }, 900, 'swing', function () {
-     window.location.hash = target;
+        window.location.hash = target;
     });
 });
 
 // code hilight
-
-$(document).ready(function(){
+$(document).ready(function () {
     hljs.initHighlightingOnLoad();
 });
