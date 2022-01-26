a772fb8baac0c68cb573ef7dc5c4ca9050d57199
Sava <sava41@gmail.com>
2022-01-26 09:48:56 -0500

    added webp backups for homepage mp4s

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,26 +1,3 @@
-// mp4 img attributes
-var videoAttr = { 'autoplay': true, 'loop': true, 'muted': true, 'playsinline': true, 'poster': "images/loading.svg"};
-var imgMP4s = Array.prototype.map.call( document.querySelectorAll('img[src*=".mp4"]'), function(img){
-
-    var src = img.src;
-    img.src = null;
-
-    img.addEventListener('error', function(e){
-        console.log('MP4 in image not supported. Replacing with video', e); 
-        var video = document.createElement('video');
-
-        for (var key in videoAttr) { video.setAttribute(key, videoAttr[key]); }
-
-        for (var imgAttr = img.attributes, len = imgAttr.length, i = 0; i < len; i++) { 
-            video.setAttribute(imgAttr[i].name,  imgAttr[i].value); 
-        }
-
-        img.parentNode.insertBefore(video, img);
-        img.parentNode.removeChild(img);
-    });
-
-    img.src = src;
-});
 
 
 // code highlight
